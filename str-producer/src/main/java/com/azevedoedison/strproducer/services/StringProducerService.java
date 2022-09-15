dc253e0b53e5c57491c3ba660261d9c43b2574d0
azevedoedison <azevedoedison@gmail.com>
2022-09-15 17:28:53 -0300

    second commit

---
+++ str-producer/src/main/java/com/azevedoedison/strproducer/services/StringProducerService.java
@@ -0,0 +1,17 @@
+package com.azevedoedison.strproducer.services;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StringProducerService {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void sendMessage(String message) {
+		kafkaTemplate.send("str-topic", message);
+	}
+}
